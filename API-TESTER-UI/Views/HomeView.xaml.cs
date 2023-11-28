f7b82c7fcb0927384e65d55d5ff586a2baf92dc0
jemas <sjemai@campsystems.com>
2023-11-28 08:04:08 -0600

    adding Auth0 code

--- API-TESTER-UI/Views/HomeView.xaml.cs
+++ API-TESTER-UI/Views/HomeView.xaml.cs
@@ -2,18 +2,14 @@
 using API_TESTER_UI.Session;
 using API_TESTER_UI.Utilities.UserManagement;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Auth0.OidcClient;
+using System.Collections.Generic;
+using System.Configuration;
+using IdentityModel.OidcClient.Browser;
+using IdentityModel.OidcClient;
+using System.Text;
 
 namespace API_TESTER_UI.Views
 {
@@ -28,9 +24,19 @@ namespace API_TESTER_UI.Views
         public string _Password { get; set; }
         public string _CwsUrl { get; set; }
         public string _SessionToken { get; set; }
+        private Auth0Client client;
 
         public object NavigationService { get; private set; }
 
+        readonly string[] _connectionNames = new string[]
+        {
+            "Username-Password-Authentication",
+            "google-oauth2",
+            "twitter",
+            "facebook",
+            "github",
+            "windowslive"
+        };
 
         public HomeView()
         {
@@ -58,9 +64,84 @@ namespace API_TESTER_UI.Views
             }
         }
 
+        //public async void LoginButton_OnClickAuth(object sender, RoutedEventArgs e)
+        //{
+        //    string domain = ConfigurationManager.AppSettings["Auth0:Domain"];
+        //    string clientId = ConfigurationManager.AppSettings["Auth0:ClientId"];
+
+        //    client = new Auth0Client(new Auth0ClientOptions
+        //    {
+        //        Domain = domain,
+        //        ClientId = clientId
+        //    });
+
+        //    var extraParameters = new Dictionary<string, string>();
+
+        //    if (!string.IsNullOrEmpty(connectionNameComboBox.Text))
+        //        extraParameters.Add("connection", connectionNameComboBox.Text);
+
+        //    if (!string.IsNullOrEmpty(audienceTextBox.Text))
+        //        extraParameters.Add("audience", audienceTextBox.Text);
+
+        //    DisplayResult(await client.LoginAsync(extraParameters: extraParameters));
+        //}
+        //private void DisplayResult(LoginResult loginResult)
+        //{
+        //    // Display error
+        //    if (loginResult.IsError)
+        //    {
+        //        resultTextBox.Text = loginResult.Error;
+        //        return;
+        //    }
+
+        //    logoutButton.Visibility = Visibility.Visible;
+        //    loginButton.Visibility = Visibility.Collapsed;
+
+        //    // Display result
+        //    StringBuilder sb = new StringBuilder();
+
+        //    sb.AppendLine("Tokens");
+        //    sb.AppendLine("------");
+        //    sb.AppendLine($"id_token: {loginResult.IdentityToken}");
+        //    sb.AppendLine($"access_token: {loginResult.AccessToken}");
+        //    sb.AppendLine($"refresh_token: {loginResult.RefreshToken}");
+        //    sb.AppendLine();
+
+        //    sb.AppendLine("Claims");
+        //    sb.AppendLine("------");
+        //    foreach (var claim in loginResult.User.Claims)
+        //    {
+        //        sb.AppendLine($"{claim.Type}: {claim.Value}");
+        //    }
+
+        //    resultTextBox.Text = sb.ToString();
+        //}
+
+        //private void Window_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    connectionNameComboBox.ItemsSource = _connectionNames;
+        //}
+
+        //private async void LogoutButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    BrowserResultType browserResult = await client.LogoutAsync();
+
+        //    if (browserResult != BrowserResultType.Success)
+        //    {
+        //        resultTextBox.Text = browserResult.ToString();
+        //        return;
+        //    }
+
+        //    logoutButton.Visibility = Visibility.Collapsed;
+        //    loginButton.Visibility = Visibility.Visible;
+
+        //    audienceTextBox.Text = "";
+        //    resultTextBox.Text = "";
+        //    connectionNameComboBox.ItemsSource = _connectionNames;
+        //}
+        // ******************************************************************************************
         private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
         {
-            LoginResponse sessionToken = null;
             var dateNow = DateTime.Now;
             try
             {
@@ -78,7 +159,7 @@ namespace API_TESTER_UI.Views
                         LoginPassword = LoginPasswordText.Password
                     };
                     var sessionClient = SoapClient.GetSessionSoapClient(cwsUrlText.Text);
-                    sessionToken = await sessionClient.LoginAsync(corridorLoginData);//login.Login(schemaAliasNameText.Text, LoginID.Text, LoginPasswordText.Password,cwsUrlText.Text);
+                    LoginResponse sessionToken = await sessionClient.LoginAsync(corridorLoginData);
 
                     if (sessionToken.SessionToken != null)
                     {
